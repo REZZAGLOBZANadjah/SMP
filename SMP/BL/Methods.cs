fd8eb1512de80ab2db18139a87f73408b6340244
REZZAG LOBZA Nadjah <39984879+REZZAGLOBZANadjah@users.noreply.github.com>
2021-09-27 17:22:36 +0100

    Add files via upload

---
+++ SMP/BL/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SMP;
+
+namespace SMP.BL
+{
+
+    class Methods
+    {
+        public MemoryStream ms = new MemoryStream();
+        public byte[] by;
+        // convert to byte
+        public byte[]convertByte()
+        {
+            return ms.ToArray();
+        }
+        // convert byte to image
+        public MemoryStream convertImage()
+        {
+            ms = new MemoryStream(by);
+            return ms;
+        }
+
+
+    }
+}
